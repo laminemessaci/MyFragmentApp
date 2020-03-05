27b9e12d9ad4ed3eba74dcab59770d3cd6495f9c
laminemessaci <Aya22062013>
2020-03-05 17:05:39 +0100

    update

--- app/src/main/java/com/lamine/myfragmentapp/MainFragment.java
+++ app/src/main/java/com/lamine/myfragmentapp/MainFragment.java
@@ -1,7 +1,9 @@
 package com.lamine.myfragmentapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,7 +14,7 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment implements View.OnClickListener{
+public class MainFragment extends Fragment implements View.OnClickListener {
 
     //2 - Declare callback
     private OnButtonClickedListener mCallback;
@@ -22,27 +24,42 @@ public class MainFragment extends Fragment implements View.OnClickListener{
         public void onButtonClicked(View view);
     }
 
-    public MainFragment () {
-        // Required empty public constructor
-    }
+    // --------------
 
 
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container,
-                              Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         //Inflate the layout of MainFragment
         View result=inflater.inflate(R.layout.fragment_main, container, false);
+
         //Set onClickListener to button "SHOW ME DETAILS"
         result.findViewById(R.id.fragment_main_button).setOnClickListener(this);
 
         return result;
     }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // 4 - Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
+    }
+
+    // --------------
+    // ACTIONS
+    // --------------
 
     @Override
-    public void onClick (View v) {
-
+    public void onClick(View v) {
+        // 5 - Spread the click to the parent activity
+        mCallback.onButtonClicked(v);
     }
+
+    // --------------
+    // FRAGMENT SUPPORT
+    // --------------
 
     // 3 - Create callback to parent activity
     private void createCallbackToParentActivity(){
