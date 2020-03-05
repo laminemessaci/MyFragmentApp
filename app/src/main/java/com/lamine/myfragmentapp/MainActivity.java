782c8907e5e5bde19cc5acc8213af1187bc74f60
laminemessaci <Aya22062013>
2020-03-05 17:12:54 +0100

    Implements Mainfragement.OnButtonClickedListener in Main Activity

--- app/src/main/java/com/lamine/myfragmentapp/MainActivity.java
+++ app/src/main/java/com/lamine/myfragmentapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.lamine.myfragmentapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onButtonClicked (View view) {
+        Log.e (getClass ().getSimpleName (), " Button Clicked :))!!");
     }
 }
