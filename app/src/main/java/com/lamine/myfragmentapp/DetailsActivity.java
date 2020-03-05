f1c7afd204eb6f20cf30c32855d539dc7cffe7c4
laminemessaci <Aya22062013>
2020-03-05 19:02:29 +0100

    Transforme interface with Dynamic methode (Création des conteneurs FrameLayout)

--- app/src/main/java/com/lamine/myfragmentapp/DetailsActivity.java
+++ app/src/main/java/com/lamine/myfragmentapp/DetailsActivity.java
@@ -6,9 +6,34 @@ import android.os.Bundle;
 
 public class DetailsActivity extends AppCompatActivity {
 
+    // 1 - Declare detail fragment
+    private DetailFragment detailFragment;
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_details);
+
+        // 2 - Configure and show home fragment
+        this.configureAndShowDetailFragment();
+    }
+
+    // --------------
+    // FRAGMENTS
+    // --------------
+
+    private void configureAndShowDetailFragment(){
+        // A - Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        detailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_detail);
+
+        if (detailFragment == null) {
+            // B - Create new main fragment
+            detailFragment = new DetailFragment();
+            // C - Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_detail, detailFragment)
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/lamine/myfragmentapp/MainActivity.java
+++ app/src/main/java/com/lamine/myfragmentapp/MainActivity.java
@@ -9,10 +9,20 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
 
+    // 1 - Declare main fragment
+    private MainFragment mainFragment;
+
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+
+        // 2 - Configure and show home fragment
+        this.configureAndShowMainFragment();
+
+
     }
 
 
@@ -21,5 +31,24 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnBu
         Log.e (getClass ().getSimpleName (), " Button Clicked :))!!");
 
         startActivity(new Intent (this, DetailsActivity.class));
+    }
+
+    // --------------
+    // FRAGMENTS
+    // --------------
+
+    private void configureAndShowMainFragment(){
+
+        // A - Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
+        mainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_main);
+
+        if (mainFragment == null) {
+            // B - Create new main fragment
+            mainFragment = new MainFragment();
+            // C - Add it to FrameLayout container
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_main, mainFragment)
+                    .commit();
+        }
     }
 }
