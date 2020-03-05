b1713bb92da99a3e2d810f4ddd2c887135029bac
laminemessaci <Aya22062013>
2020-03-05 16:52:47 +0100

    update

--- app/src/main/java/com/lamine/myfragmentapp/MainFragment.java
+++ app/src/main/java/com/lamine/myfragmentapp/MainFragment.java
@@ -22,8 +22,9 @@ public class MainFragment extends Fragment implements View.OnClickListener{
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container,
                               Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate (R.layout.fragment_main, container, false);
+       View result = inflater.inflate (R.layout.fragment_main, container, false);
+       result.findViewById (R.id.fragment_main_button).setOnClickListener (this);
+       return result;
     }
 
 
